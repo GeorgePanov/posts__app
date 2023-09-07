fbf035b3829988c9a987a92999749d376d7f2b4f
George Panov <georgepanov19@gmail.com>
2023-09-08 00:58:24 +0300

    custom hooks

--- firstApp/src/App.js
+++ firstApp/src/App.js
@@ -1,13 +1,11 @@
-import React, { useMemo, useState } from "react";
-// import Counter from "./components/Counter";
-// import Text__input from "./components/Text__input";
-// import ClassCounter from "./components/ClassCounter";
+import React, { useState } from "react";
 import './styles/App.css';
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import { usePosts } from "./hooks/usePosts";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -15,37 +13,20 @@ function App() {
         { id: 2, title: 'Дизайн iPhone 15', body: '«Прошкам» тоже достанется: рамки дисплея уменьшатся до 1,5 мм со всех сторон. Apple постепенно идёт к отказу от рамок, а это шаг на пути к цели. К слову, пока ни у какого смартфона нет таких узких рамок. ' },
         { id: 3, title: 'Дизайн iPhone 15', body: 'Ещё мы ожидаем, что задняя панель всех iPhone 15 будет слегка загнутой. Аналогичную фишку уже применила Nothing в своём Phone (2)' },
         { id: 4, title: 'Цвета iPhone 15', body: 'Пока нет информации, какие цвета будут представлены. Ходят слухи, что «прошки» получат глубокий красный, а базовые версии — насыщенные розовый и голубой цвета, как на рендере MacRumors выше. Но информация о них была очень давно.' },
-        // { id: 5, title: 'Цвета iPhone 15', body: '' }
     ])
-
 
     const [filter, setFilter] = useState({ sort: '', query: '' })
     const [modal, setModal] = useState(false)
-
-    const sortedPosts = useMemo(() => {
-        console.log('Work!');
-        if (filter.sort) {
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-        }
-        return posts;
-    }, [filter.sort, posts])
-
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.body.toLowerCase().includes(filter.query))
-    }, [filter.query, sortedPosts])
-
-
-
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
     }
 
+    // Получаем post из дочернего компонента
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
     }
-
-
 
 
     return (
@@ -62,7 +43,7 @@ function App() {
                 setFilter={setFilter}
             />
             <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Посты' />
-        </div >
+        </div>
     )
 }
 
