99c4934a45353b8c030d6ffd027ea15b4702ef13
George Panov <georgepanov19@gmail.com>
2023-09-13 02:21:53 +0300

    React router, BrowserRouter, Routes, Navigate

---
+++ firstApp/src/components/AppRouter.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Routes, Navigate } from "react-router-dom"
+import About from '../pages/About';
+import Posts from '../pages/Posts';
+import Error from '../pages/Error';
+
+const AppRouter = () => {
+    return (
+        <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/posts" element={<Posts />} />
+            <Route path="/" />
+
+            {/* Если страница будет не найдена, то нужно будет её преадресовать на страницу error */}
+            <Route path="/error" element={<Error />} />
+            <Route path="*" element={<Navigate to='error' />} />
+        </Routes>
+    );
+};
+
+export default AppRouter;
