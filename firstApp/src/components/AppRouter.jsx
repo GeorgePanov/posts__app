8cc20733778de2aad8cae4f0a01f33310d07c9fc
GeorgePanov <georgepanov19@gmail.com>
2023-09-15 00:53:35 +0300

    useContext

--- firstApp/src/components/AppRouter.jsx
+++ firstApp/src/components/AppRouter.jsx
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Route, Routes, Navigate } from "react-router-dom"
 import { privateRoutes, publicRoutes } from '../router';
+import { AuthContext } from '../context';
+import Loader from './UI/Loader/Loader';
 
 const AppRouter = () => {
-    const isAuth = true
+    const { isAuth, isLoading } = useContext(AuthContext)
+    console.log(isAuth);
+
+    if (isLoading) {
+        return <Loader />
+    }
 
     return (
         isAuth
             ?
             <Routes>
                 {privateRoutes.map(route =>
-                    <Route path={route.path} Component={route.component} />
+                    <Route path={route.path} Component={route.component} key={route.path} />
                 )}
 
                 <Route path="/" element={<Navigate to='posts' />} />
+                <Route path="/login" element={<Navigate to='../posts' />} />
                 <Route path="*" element={<Navigate to='error' />} />
             </Routes>
             :
             <Routes>
                 {publicRoutes.map(route =>
-                    <Route path={route.path} Component={route.component} />
+                    <Route path={route.path} Component={route.component} key={route.path} />
                 )}
 
                 <Route path="*" element={<Navigate to='login' />} />
